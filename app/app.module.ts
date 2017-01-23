95e27add54a08f15a4f24f69d3402d300d893ffc
Sirb Raul Cosmin <raul_cosmin_26@yahoo.com>
2017-01-23 17:13:43 +0200

    Working app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 
